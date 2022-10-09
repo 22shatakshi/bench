fb9626457e21c8075b35db91b2dba80c4e1b072f
Shatakshi <67089824+22shatakshi@users.noreply.github.com>
2022-10-09 13:27:02 -0400

    Add files via upload

--- src/signup.js
+++ src/signup.js
@@ -1,5 +1,4 @@
 import * as React from "react";
-import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
@@ -8,7 +7,6 @@ import Checkbox from '@mui/material/Checkbox';
 import Link from '@mui/material/Link';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
@@ -50,9 +48,7 @@ export default function SignUp() {
             alignItems: 'center',
           }}
         >
-          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-            <LockOutlinedIcon />
-          </Avatar>
+          <h1>BENCH</h1>
           <Typography component="h1" variant="h5">
             Sign up
           </Typography>
