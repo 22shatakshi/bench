a50faf1f21cf27383c555e6b26f54aef6016bf79
EstelleTheEmu <89495155+YEstelle@users.noreply.github.com>
2022-10-09 13:28:19 -0400

    Updated app.tsx for protected routing feature

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,15 +3,27 @@ import type { AppProps } from 'next/app'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Navbar from '../components/Navbar'
 import { AuthContextProvider } from '../context/AuthContext'
+import { useRouter } from 'next/router'
+import ProtectedRoute from '../components/ProtectedRoute'
+
+const noAuthRequired = ['/', '/login', '/signup']
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter()
 
   return (
     <AuthContextProvider>
-    <Navbar />
-    <Component {...pageProps} />
+      <Navbar />
+      {noAuthRequired.includes(router.pathname) ? (
+        <Component {...pageProps} />
+      ) : (
+        <ProtectedRoute>
+          <Component {...pageProps} />
+        </ProtectedRoute>
+      )}
     </AuthContextProvider>
   )
 }
 
 export default MyApp
+
