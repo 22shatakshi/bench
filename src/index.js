dbdf88f3a08a3b9c29301ecf6815f95cbe4649be
Shatakshi <67089824+22shatakshi@users.noreply.github.com>
2022-10-09 12:44:41 -0400

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SignUp from './signup';
+
+const element = <h1>Hello World</h1>;
+
+//console.log(element);
+ReactDOM.render(<SignUp/>, document.getElementById('root'));
