105ec77285caacd0d1aac9b2ed55e924b7780ff5
EstelleTheEmu <89495155+YEstelle@users.noreply.github.com>
2022-10-11 16:30:43 -0400

    Update firebase.ts

--- config/firebase.ts
+++ config/firebase.ts
@@ -1,5 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 //import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
@@ -19,5 +20,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const database = getFirestore(app);
 //const analytics = getAnalytics(app);
-export const auth = getAuth()
+const auth = getAuth()
+export {auth, database}
