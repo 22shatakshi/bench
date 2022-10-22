275b73b25d081e04e6082ec9777db456cd77d93a
EstelleTheEmu <89495155+YEstelle@users.noreply.github.com>
2022-10-22 14:36:44 -0400

    UI for matching page dashboard

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,7 +1,144 @@
 import React from 'react'
+import { MDBCol, MDBContainer, MDBRow, MDBCard, MDBCardText, MDBCardBody, MDBCardImage, MDBBtn, MDBTypography, MDBIcon } from 'mdb-react-ui-kit';
+
 
 const Dashboard = () => {
-  return <div>Welcome</div>
+  
+  
+  return (
+    <div className="vh-100" style={{ backgroundColor: '#eee' }}>
+      <MDBContainer className="container py-5 h-100">
+        <MDBRow className="justify-content-center align-items-center h-100">
+          <MDBCol md="12" xl="4">
+            <MDBCard style={{ borderRadius: '15px' }}>
+              <MDBCardBody className="text-center">
+                <div className="mt-3 mb-4">
+                  <MDBCardImage src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava2-bg.webp"
+                    className="rounded-circle" fluid style={{ width: '100px' }} />
+                </div>
+                <MDBTypography tag="h4">Julie L. Arsenault</MDBTypography>
+                <MDBCardText className="text-muted mb-4">
+                  @Programmer <span className="mx-2">|</span> <a href="#!">mdbootstrap.com</a>
+                </MDBCardText>
+                <div className="mb-4 pb-2">
+                  <MDBBtn outline floating>
+                    <MDBIcon fab icon="facebook" size="lg" />
+                  </MDBBtn>
+                  <MDBBtn outline floating className="mx-1">
+                    <MDBIcon fab icon="twitter" size="lg" />
+                  </MDBBtn>
+                  <MDBBtn outline floating>
+                    <MDBIcon fab icon="skype" size="lg" />
+                  </MDBBtn>
+                </div>
+                <MDBBtn rounded size="lg">
+                  Message now
+                </MDBBtn>
+                <div className="d-flex justify-content-between text-center mt-5 mb-2">
+                  <div>
+                    <MDBCardText className="mb-1 h5">8471</MDBCardText>
+                    <MDBCardText className="small text-muted mb-0">Wallets Balance</MDBCardText>
+                  </div>
+                  <div className="px-3">
+                    <MDBCardText className="mb-1 h5">8512</MDBCardText>
+                    <MDBCardText className="small text-muted mb-0">Followers</MDBCardText>
+                  </div>
+                  <div>
+                    <MDBCardText className="mb-1 h5">4751</MDBCardText>
+                    <MDBCardText className="small text-muted mb-0">Total Transactions</MDBCardText>
+                  </div>
+                </div>
+              </MDBCardBody>
+            </MDBCard>
+          </MDBCol>
+          <MDBCol md="12" xl="4">
+            <MDBCard style={{ borderRadius: '15px' }}>
+              <MDBCardBody className="text-center">
+                <div className="mt-3 mb-4">
+                  <MDBCardImage src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava2-bg.webp"
+                    className="rounded-circle" fluid style={{ width: '100px' }} />
+                </div>
+                <MDBTypography tag="h4">Julie L. Arsenault</MDBTypography>
+                <MDBCardText className="text-muted mb-4">
+                  @Programmer <span className="mx-2">|</span> <a href="#!">mdbootstrap.com</a>
+                </MDBCardText>
+                <div className="mb-4 pb-2">
+                  <MDBBtn outline floating>
+                    <MDBIcon fab icon="facebook" size="lg" />
+                  </MDBBtn>
+                  <MDBBtn outline floating className="mx-1">
+                    <MDBIcon fab icon="twitter" size="lg" />
+                  </MDBBtn>
+                  <MDBBtn outline floating>
+                    <MDBIcon fab icon="skype" size="lg" />
+                  </MDBBtn>
+                </div>
+                <MDBBtn rounded size="lg">
+                  Message now
+                </MDBBtn>
+                <div className="d-flex justify-content-between text-center mt-5 mb-2">
+                  <div>
+                    <MDBCardText className="mb-1 h5">8471</MDBCardText>
+                    <MDBCardText className="small text-muted mb-0">Wallets Balance</MDBCardText>
+                  </div>
+                  <div className="px-3">
+                    <MDBCardText className="mb-1 h5">8512</MDBCardText>
+                    <MDBCardText className="small text-muted mb-0">Followers</MDBCardText>
+                  </div>
+                  <div>
+                    <MDBCardText className="mb-1 h5">4751</MDBCardText>
+                    <MDBCardText className="small text-muted mb-0">Total Transactions</MDBCardText>
+                  </div>
+                </div>
+              </MDBCardBody>
+            </MDBCard>
+          </MDBCol>
+          <MDBCol md="12" xl="4">
+            <MDBCard style={{ borderRadius: '15px' }}>
+              <MDBCardBody className="text-center">
+                <div className="mt-3 mb-4">
+                  <MDBCardImage src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava2-bg.webp"
+                    className="rounded-circle" fluid style={{ width: '100px' }} />
+                </div>
+                <MDBTypography tag="h4">Julie L. Arsenault</MDBTypography>
+                <MDBCardText className="text-muted mb-4">
+                  @Programmer <span className="mx-2">|</span> <a href="#!">mdbootstrap.com</a>
+                </MDBCardText>
+                <div className="mb-4 pb-2">
+                  <MDBBtn outline floating>
+                    <MDBIcon fab icon="facebook" size="lg" />
+                  </MDBBtn>
+                  <MDBBtn outline floating className="mx-1">
+                    <MDBIcon fab icon="twitter" size="lg" />
+                  </MDBBtn>
+                  <MDBBtn outline floating>
+                    <MDBIcon fab icon="skype" size="lg" />
+                  </MDBBtn>
+                </div>
+                <MDBBtn rounded size="lg">
+                  Message now
+                </MDBBtn>
+                <div className="d-flex justify-content-between text-center mt-5 mb-2">
+                  <div>
+                    <MDBCardText className="mb-1 h5">8471</MDBCardText>
+                    <MDBCardText className="small text-muted mb-0">Wallets Balance</MDBCardText>
+                  </div>
+                  <div className="px-3">
+                    <MDBCardText className="mb-1 h5">8512</MDBCardText>
+                    <MDBCardText className="small text-muted mb-0">Followers</MDBCardText>
+                  </div>
+                  <div>
+                    <MDBCardText className="mb-1 h5">4751</MDBCardText>
+                    <MDBCardText className="small text-muted mb-0">Total Transactions</MDBCardText>
+                  </div>
+                </div>
+              </MDBCardBody>
+            </MDBCard>
+          </MDBCol>
+        </MDBRow>
+      </MDBContainer>
+    </div>
+  );
 }
 
 export default Dashboard
