4304fad9f07853b6adba186e686ffa87da51975d
EstelleTheEmu <89495155+YEstelle@users.noreply.github.com>
2022-11-03 19:02:14 -0400

    Update [id].js

--- pages/users/[id].js
+++ pages/users/[id].js
@@ -2,27 +2,30 @@ import { collection, getDoc, getDocs, doc, getFirestore } from "firebase/firesto
 import { database } from '../../config/firebase';
 import {serializeDocumentSnapshot, serializeQuerySnapshot, deserializeDocumentSnapshot, deserializeDocumentSnapshotArray} from "firestore-serializers";
 import userlistRequest from "../../data/userList"
+import { useEffect, useState } from "react";
+import { getDownloadURL, getStorage, ref } from 'firebase/storage'
+import {
+    MDBCol,
+    MDBContainer,
+    MDBRow,
+    MDBCard,
+    MDBCardText,
+    MDBCardBody,
+    MDBCardImage,
+    MDBProgress,
+    MDBProgressBar,
+    MDBListGroup,
+    MDBListGroupItem,
+    MDBIcon,
+    MDBBtn
+  } from 'mdb-react-ui-kit';
+  import { useRouter } from "next/router";
 //npm install firestore-serializers
 
 export const getStaticPaths = async () => {
     let users = await userlistRequest()
-    // const querySnapshot = await getDocs(collection(database, "username"));
-    // try {
-    //     querySnapshot.forEach((doc) => {
-    //         let data = doc.data()
-    //         users.push({
-    //             uid: data.uid,
-    //             username: data.username,
-    //             email: data.email
-    //         })
-    //     })
-    // } catch (e) {
-    //     console.log(e)
-    // }
-    //console.log("Users:", users)
     const paths = users.map((user) => ({
         params: {id: user.uid}
-
     }))
     return {
         paths,
@@ -47,12 +50,167 @@ export const getStaticProps = async (context) => {
 
 
 const Profile = ({ user }) => {
+    const router = useRouter()
     const data = deserializeDocumentSnapshot(user, getFirestore())
+    const [loading, setLoading] = useState(true)
+    const storage = getStorage();
+    var imageLink
+    useEffect (() => {
+        const fetchProfileImage = async () => {
+            try {
+                imageLink = await getDownloadURL(ref(storage, String(user.uid)))
+            } catch(e) {
+                imageLink = getDownloadURL(ref(storage, 'xsFnrxqQXcPYe9NpEQW0A6wpl5Z2'))
+            }
+            console.log(data.name)
+            setLoading(false)
+        }
+        fetchProfileImage()
+    }, [])
+    
+
     return (
-        <div>
-            <h1>{ data.get("username")}</h1>
-            <h1>{ data.get("email")}</h1>
-        </div>
+        <section style={{ backgroundColor: '#eee' }}>
+        <MDBContainer className="py-5">
+        {loading ? (
+            <div> Loading </div>
+        ) : (
+            <><MDBRow>
+                            <MDBCol lg="4">
+                                <MDBCard className="mb-4">
+                                    <MDBCardBody className="text-center">
+                                        <MDBCardImage
+                                            src={imageLink}
+                                            alt="avatar"
+                                            className="rounded-circle"
+                                            style={{ width: '150px' }}
+                                            fluid />
+                                        <p className="text-muted mb-1">{data.get("username")}</p>
+                                        <p className="text-muted mb-4">Status: {data.get("status")}</p>
+                                    </MDBCardBody>
+                                </MDBCard>
+
+                                <MDBCard className="mb-4 mb-lg-0">
+                                    <MDBCardBody className="p-0">
+                                        <MDBListGroup flush className="rounded-3">
+                                            <MDBListGroupItem className="d-flex justify-content-between align-items-center p-3">
+                                                <MDBIcon fas icon="globe fa-lg text-warning" />
+                                                <MDBCardText><a href="https://github.com/22shatakshi/bench" style={{ color: '#000000' }}>Website</a></MDBCardText>
+                                            </MDBListGroupItem>
+                                            <MDBListGroupItem className="d-flex justify-content-between align-items-center p-3">
+                                                <MDBIcon fab icon="twitter fa-lg" style={{ color: '#55acee' }} />
+                                                <MDBCardText><a href="https://github.com/22shatakshi/bench" style={{ color: '#000000' }}>@shatakshi</a></MDBCardText>
+                                            </MDBListGroupItem>
+                                            <MDBListGroupItem className="d-flex justify-content-between align-items-center p-3">
+                                                <MDBIcon fab icon="instagram fa-lg" style={{ color: '#ac2bac' }} />
+                                                <MDBCardText><a href="https://github.com/22shatakshi/bench" style={{ color: '#000000' }}>@shatakshi</a></MDBCardText>
+                                            </MDBListGroupItem>
+                                            <MDBListGroupItem className="d-flex justify-content-between align-items-center p-3">
+                                                <MDBIcon fab icon="facebook fa-lg" style={{ color: '#3b5998' }} />
+                                                <MDBCardText><a href="https://github.com/22shatakshi/bench" style={{ color: '#000000' }}>@shatakshi</a></MDBCardText>
+                                            </MDBListGroupItem>
+                                        </MDBListGroup>
+                                    </MDBCardBody>
+                                </MDBCard>
+                            </MDBCol>
+                            <MDBCol lg="8">
+                                <MDBCard className="mb-4">
+                                    <MDBCardBody>
+                                        <MDBRow>
+                                            <MDBCol sm="3">
+                                                <MDBCardText>Name</MDBCardText>
+                                            </MDBCol>
+                                            <MDBCol sm="9">
+                                                <MDBCardText className="text-muted">{data.get("name")}</MDBCardText>
+                                            </MDBCol>
+                                        </MDBRow>
+                                        <hr />
+                                        <MDBRow>
+                                            <MDBCol sm="3">
+                                                <MDBCardText>Email</MDBCardText>
+                                            </MDBCol>
+                                            <MDBCol sm="9">
+                                                <MDBCardText className="text-muted">{data.get("email")}</MDBCardText>
+                                            </MDBCol>
+                                        </MDBRow>
+                                        <hr />
+                                        <MDBRow>
+                                            <MDBCol sm="3">
+                                                <MDBCardText>Phone</MDBCardText>
+                                            </MDBCol>
+                                            <MDBCol sm="9">
+                                                <MDBCardText className="text-muted">(123) 456-7890</MDBCardText>
+                                            </MDBCol>
+
+                                        </MDBRow>
+                                        <hr />
+                                        <MDBRow>
+                                            <MDBCol sm="3">
+                                                <MDBCardText>Address</MDBCardText>
+                                            </MDBCol>
+                                            <MDBCol sm="9">
+                                                <MDBCardText className="text-muted">West Lafayette, IN 47906</MDBCardText>
+                                            </MDBCol>
+                                        </MDBRow>
+                                    </MDBCardBody>
+                                </MDBCard>
+
+                                <MDBRow>
+                                    <MDBCol md="6">
+                                        <MDBCard className="mb-4 mb-md-0">
+                                            <MDBCardBody>
+                                                <MDBCardText className="mb-4">My Sports</MDBCardText>
+                                                <MDBCardText className="mb-1" style={{ fontSize: '.77rem' }}>Tennis</MDBCardText>
+                                                <MDBProgress className="rounded">
+                                                    <MDBProgressBar width={80} valuemin={0} valuemax={100} />
+                                                </MDBProgress>
+
+                                                <MDBCardText className="mt-4 mb-1" style={{ fontSize: '.77rem' }}>Volleyball</MDBCardText>
+                                                <MDBProgress className="rounded">
+                                                    <MDBProgressBar width={72} valuemin={0} valuemax={100} />
+                                                </MDBProgress>
+
+                                                <MDBCardText className="mt-4 mb-1" style={{ fontSize: '.77rem' }}>Badminton</MDBCardText>
+                                                <MDBProgress className="rounded">
+                                                    <MDBProgressBar width={89} valuemin={0} valuemax={100} />
+                                                </MDBProgress>
+
+                                                <MDBCardText className="mt-4 mb-1" style={{ fontSize: '.77rem' }}>Football</MDBCardText>
+                                                <MDBProgress className="rounded">
+                                                    <MDBProgressBar width={55} valuemin={0} valuemax={100} />
+                                                </MDBProgress>
+
+                                                <MDBCardText className="mt-4 mb-1" style={{ fontSize: '.77rem' }}>Basketball</MDBCardText>
+                                                <MDBProgress className="rounded">
+                                                    <MDBProgressBar width={66} valuemin={0} valuemax={100} />
+                                                </MDBProgress>
+                                            </MDBCardBody>
+                                        </MDBCard>
+                                    </MDBCol>
+
+                                    <MDBCol md="6">
+                                        <MDBCard className="mb-4 mb-md-0">
+                                            <MDBCardBody>
+                                                <MDBCardText className="mb-4">Average Rating</MDBCardText>
+
+                                                <MDBProgress className="rounded">
+                                                    <MDBProgressBar width={89} valuemin={0} valuemax={100} />
+                                                </MDBProgress>
+                                                <hr className="my-4" />
+                                                <MDBCardText className="mb-4">Reviews</MDBCardText>
+                                                <MDBCardImage width="35" src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/avatar-2.webp" alt="avatar" className="rounded-circle me-1" />
+                                                <MDBCardText className="mt-4 mb-1" style={{ fontSize: '.77rem' }}>They are enthusaitic about volleyball and showed up on time. I had a lot of fun playing with them and I am surely looking foreard to more games with them.</MDBCardText>
+                                            </MDBCardBody>
+                                        </MDBCard>
+                                    </MDBCol>
+                                </MDBRow>
+                            </MDBCol>
+                        </MDBRow>
+                        <MDBBtn style={{float: 'right'}} onClick={() => router.push('/dashboard')}>
+                            Go Back
+                        </MDBBtn></>)}
+        </MDBContainer>
+        </section>
     )
 }
 
