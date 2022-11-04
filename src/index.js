c26ccb7ad2dbe7b3a4f0570943ddc8180fdae475
Shatakshi <67089824+22shatakshi@users.noreply.github.com>
2022-11-04 12:23:37 -0400

    Delete src directory

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import SignUp from './signup';
-
-const element = <h1>Hello World</h1>;
-
-//console.log(element);
-ReactDOM.render(<SignUp/>, document.getElementById('root'));
