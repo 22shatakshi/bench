30af1db2103e598184a71ba1ea5fc393dd91f03a
Shatakshi <67089824+22shatakshi@users.noreply.github.com>
2022-11-26 15:21:36 -0500

    Button for clear chat

--- pages/chat/components/Appbar.jsx
+++ pages/chat/components/Appbar.jsx
@@ -7,6 +7,9 @@ import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
 import MenuItem from '@mui/material/MenuItem';
 import Menu from '@mui/material/Menu';
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+
 
 const Appbar = () => {
     const { data } = useContext(ChatContext);
@@ -23,42 +26,44 @@ const Appbar = () => {
 
     return (
         <div>
+            <Box sx={{ flexGrow: 1 }}>
                 <AppBar position="static">
                     <Toolbar>
-                        <IconButton
-                            size="large"
-                            aria-label="account of current user"
-                            aria-controls="menu-appbar"
-                            aria-haspopup="true"
-                            color="inherit"
-                            onClick={handleMenu}
-                        >
-                            <Avatar alt="Remy Sharp" src={data.user?.photoURL} />
-                        </IconButton>
-                        <Menu
-                            id="menu-appbar"
-                            anchorEl={anchorEl}
-                            anchorOrigin={{
-                            vertical: 'top',
-                            horizontal: 'left',
-                            }}
-                            keepMounted
-                            transformOrigin={{
-                            vertical: 'top',
-                            horizontal: 'left',
-                            }}
-                            open={Boolean(anchorEl)}
-                            onClose={handleClose}
-                        >
-                            <MenuItem>Block</MenuItem>
-                            <MenuItem>Report</MenuItem>
-                        </Menu>
-                        <Typography>
-                        {data.user?.displayName}
+                    <IconButton
+                        size="large"
+                        aria-label="account of current user"
+                        aria-controls="menu-appbar"
+                        aria-haspopup="true"
+                        color="inherit"
+                        onClick={handleMenu}
+                    >
+                        <Avatar alt="Remy Sharp" src={data.user?.photoURL} />
+                    </IconButton>
+                    <Menu
+                        id="menu-appbar"
+                        anchorEl={anchorEl}
+                        anchorOrigin={{
+                        vertical: 'top',
+                        horizontal: 'left',
+                        }}
+                        keepMounted
+                        transformOrigin={{
+                        vertical: 'top',
+                        horizontal: 'left',
+                        }}
+                        open={Boolean(anchorEl)}
+                        onClose={handleClose}
+                    >
+                        <MenuItem>Block</MenuItem>
+                        <MenuItem>Report</MenuItem>
+                    </Menu>
+                    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                    {data.user?.displayName}
                     </Typography>
+                    <Button color="inherit">Clear Chat</Button>
                     </Toolbar>
                 </AppBar>
-
+                </Box>
         </div>
     )
 }
