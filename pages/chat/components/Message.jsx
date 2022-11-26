1a452ec9b9dd9013c6a5c7c8dfae901c2a65afdd
EstelleTheEmu <89495155+YEstelle@users.noreply.github.com>
2022-11-26 17:15:26 -0500

    Send image through chat function complete

--- pages/chat/components/Message.jsx
+++ pages/chat/components/Message.jsx
@@ -1,19 +1,44 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useRef } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 import { useAuth } from "../../../context/AuthContext"
 import { ChatContext } from "../../../context/ChatContext"
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
+import ImageListItem from '@mui/material/ImageListItem';
 import Grid from '@material-ui/core/Grid';
 
+const useStyles = makeStyles({
+    imageOwner: {
+      width: 200,
+      height: 150,
+      float: "float:right",
+    },
+    notOwner: {
+      width: 200,
+      height: 150,
+      float: "float:left",
+    }
+});
+
 const Message = ({message}) => {
+    const classes = useStyles()
     const { user } = useAuth();
     const { data } = useContext(ChatContext)
+
+    const ref = useRef();
+    
+    useEffect(() => {
+        ref.current?.scrollIntoView({ behavior: "smooth" });
+    }, [message])
 
     return (
         <ListItem key="1">
             <Grid container>
                 <Grid item xs={12}>
-                    <ListItemText style={{float: message.sender === user.uid ? "right" : "left"}} primary={message.text}></ListItemText>
+                    <p><ListItemText style={{float: message.sender === user.uid ? "right" : "left"}} primary={message.text}></ListItemText></p>
+                    {message.file && <img
+                        src={message.file} className={message.sender === user.uid? classes.imageOwner : classes.notOwner}
+                    />}
                 </Grid>
             </Grid>
         </ListItem>
